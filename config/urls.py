da76828b8c5ddcb16b6479782d200ef1874c8433
Aleksandr Suvorov <suvorov@steelkiwi.com>
2017-12-09 16:13:30 +0000

    set login as index page

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 from django.contrib import admin
 
+from allauth.account.views import login
+
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^$', login, name='home'),
     url(settings.ADMIN_URL, include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^users/', include('qadip.users.urls', namespace='users')),
